546b19d6d0102e74960893f8ee7453153c0d116b
chaosxingxc-orion <chaos.xing.xc@gmail.com>
2026-04-11 22:52:30 +0800

    feat(config): export new config modules from hi_agent.config package

--- hi_agent/config/__init__.py
+++ hi_agent/config/__init__.py
@@ -1,9 +1,20 @@
 """Centralized configuration system for TRACE subsystems."""
 
-from hi_agent.config.builder import SystemBuilder
 from hi_agent.config.trace_config import TraceConfig
+from hi_agent.config.builder import SystemBuilder
+from hi_agent.config.stack import ConfigStack
+from hi_agent.config.validator import ConfigValidator, ConfigValidationError
+from hi_agent.config.profile import deep_merge, load_profile_file, profile_path_for
+from hi_agent.config.watcher import ConfigFileWatcher
 
 __all__ = [
-    "SystemBuilder",
     "TraceConfig",
+    "SystemBuilder",
+    "ConfigStack",
+    "ConfigValidator",
+    "ConfigValidationError",
+    "ConfigFileWatcher",
+    "deep_merge",
+    "load_profile_file",
+    "profile_path_for",
 ]
